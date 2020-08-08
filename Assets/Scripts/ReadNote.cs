43dfa62803c9694d14bf3b68668238fce9d237ab
DanielJimenezMorales <danieljimenezmorales3@gmail.com>
2020-08-08 13:29:47 +0200

    Creación del Script ReadNote

---
+++ Assets/Scripts/ReadNote.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReadNote : MonoBehaviour
+{
+    public Transform Player;
+    public float minDist;
+    private bool isReading;
+    private float dist = 5.0f;
+
+    private void Start()
+    {
+        isReading = false;
+    }
+
+    private void Update()
+    {
+        dist = Vector3.Distance(Player.position, this.transform.position);
+
+        if(dist <= minDist)
+        {
+            if(Input.GetButtonDown("ReadNote") && isReading == false)
+            {
+                isReading = true;
+                Debug.Log("Estás leyendo una nota");
+            }
+        }
+    }
+}
