1b3fddc22fce1ce6ca9c3de51f00d4a0a0ae66e9
DanielJimenezMorales <danieljimenezmorales3@gmail.com>
2020-08-09 23:47:55 +0200

    Mejorando las notas

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Type
+{
+    key,
+    note
+}
+
+public class Collectable : MonoBehaviour
+{
+    public string firstPageText;
+    public string secondPageText;
+    public Type currentType;
+}
