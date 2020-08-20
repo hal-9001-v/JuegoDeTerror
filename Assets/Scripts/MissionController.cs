8eb8c40949d59c7dd97bb7ca5d9de89c9bf42617
DanielJimenezMorales <danieljimenezmorales3@gmail.com>
2020-08-20 20:53:48 +0200

    Pull de la primera prueba del Controlador de Misiones

---
+++ Assets/Scripts/MissionController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using TMPro;
+
+public class MissionController : MonoBehaviour
+{
+    [TextArea(0, 3)] public List<string> missionList = new List<string>();
+    private TextMeshProUGUI missionCanvasText;
+
+    private int missionIndex;
+
+    private void Start()
+    {
+        missionCanvasText = GetComponent<TextMeshProUGUI>();
+        missionIndex = -1;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            NextMission();
+        }
+    }
+
+    public void NextMission()
+    {
+        if((missionIndex + 1) < missionList.Count())
+        {
+            missionIndex++;
+            missionCanvasText.text = missionList[missionIndex];
+        }
+    }
+}
