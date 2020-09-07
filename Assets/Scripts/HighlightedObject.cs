780233d9c6dfb1c42c8875fc49ef96250ea03c6e
DanielJimenezMorales <danieljimenezmorales3@gmail.com>
2020-09-07 13:39:10 +0200

    Empezando a programar un sistema de resalto de objetos interactuables

---
+++ Assets/Scripts/HighlightedObject.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightedObject : MonoBehaviour
+{
+    public bool isHighlighted = false;
+    public bool openObject = false;
+    private Light light;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GetComponent<Light>();
+        light.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameManager.sharedInstance.currentGameState == GameState.inGame)
+        {
+            if(isHighlighted == false && openObject == false)
+            {
+                //StartCoroutine(Highlighting());
+                isHighlighted = true;
+                light.enabled = true;
+            }
+
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                openObject = true;
+            }
+
+            if(openObject)
+            {
+                //StopCoroutine(Highlighting());
+                isHighlighted = false;
+                light.enabled = false;
+            }
+        }
+    }
+
+    /*public IEnumerator Highlighting()
+    {
+
+    }*/
+}
