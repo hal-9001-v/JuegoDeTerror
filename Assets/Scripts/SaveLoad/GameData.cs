8a52d4f51e57b7abc70a387423ffa23c72a0aa46
DanielJimenezMorales <danieljimenezmorales3@gmail.com>
2020-09-13 18:15:08 +0200

    SaveLoadSystem1

---
+++ Assets/Scripts/SaveLoad/GameData.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[Serializable]
+public class GameData
+{
+    public PlayerData myPlayerData;
+
+    public GameData()
+    {
+
+    }
+}
+
+[Serializable]
+public class PlayerData
+{
+    public float[] playerPosition;
+
+    public PlayerData(Vector3 position)
+    {
+        this.playerPosition[0] = position.x;
+        this.playerPosition[1] = position.y;
+        this.playerPosition[2] = position.z;
+    }
+}
