1eac8f0d414776204950f86e8bce28a88adccc0d
DanielJimenezMorales <danieljimenezmorales3@gmail.com>
2020-09-17 15:55:21 +0200

    Añadido sistema de combinación puzzle

---
+++ Assets/Scripts/DigitalDisplay.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DigitalDisplay : MonoBehaviour
+{
+    public TextMeshProUGUI text;
+    public float minDist;
+    public float dist;
+    public Transform player;
+    public Canvas displayCnavas;
+    public AudioClip clickSound;
+    private AudioSource audioSource;
+    private bool displayActivated = false;
+    private string secuence = "";
+
+    private void Start()
+    {
+        displayCnavas.enabled = false;
+        audioSource = this.GetComponent<AudioSource>();
+    }
+    void Update()
+    {
+        if (GameManager.sharedInstance.currentGameState == GameState.inGame)
+        {
+            if (displayActivated == false)
+            {
+                dist = Vector3.Distance(player.position, this.transform.position);
+                if (dist <= minDist)
+                {
+                    if (Input.GetButtonDown("Interact"))
+                    {
+                        displayActivated = true;
+                        displayCnavas.enabled = true;
+                        PlayerMovement.sharedInstance.isReading = true;
+                        Cursor.lockState = CursorLockMode.None;
+                    }
+                }
+            }
+        }
+    }
+
+    public void AddDigitToSequence(int number)
+    {
+        secuence += number;
+        text.text = secuence;
+        audioSource.PlayOneShot(clickSound);
+    }
+
+    public void DeleteSecuence()
+    {
+        secuence = "";
+        text.text = secuence;
+    }
+}
