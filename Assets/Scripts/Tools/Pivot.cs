890af9f693b8e844be418021a16c55fbbaebbd3a
hal-9001-v <57011035+hal-9001-v@users.noreply.github.com>
2020-09-21 20:13:26 +0200

    Save

---
+++ Assets/Scripts/Tools/Pivot.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pivot : MonoBehaviour
+{
+    public Color colorOfPivot;
+
+    [Range(0.1f, 5)]
+    public float sizeOfPivot;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = colorOfPivot;
+        Gizmos.DrawSphere(transform.position,sizeOfPivot);
+    }
+}
