aab829b351d73047be7daa484b64d09822e29918
DanielJimenezMorales <danieljimenezmorales3@gmail.com>
2020-12-03 23:54:58 +0100

    Algunos comentarios acerca del funcionamiento de GameEventManager

--- Assets/Scripts/GameEventManager.cs
+++ Assets/Scripts/GameEventManager.cs
@@ -3,6 +3,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/*Q: HOW THIS CLASS WORKS?
+ * 
+ *R: This class represent the main and one subject.
+ * Every single class that makes any reference to this one will meant that they where subscribing to any event (They will be listeners
+ * following the Observer pattern)
+ * 
+ *Q: Want to create a new Event?
+ * 
+ *R: Create an event variable, and then its own public method.
+ *
+ *Q: Want to add some parameters to your events?
+ *
+ *R: If so, create a new class which inherits from EventArgs (only if you use EventHandler events) and define there your own parameters
+ */
+
 public class GameEventManager : MonoBehaviour
 {
     public static GameEventManager sharedInstance;
