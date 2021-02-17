2478040cb46f749df4692d3f8bda87f8741b05d4
hal-9001-v <57011035+hal-9001-v@users.noreply.github.com>
2021-02-17 01:56:17 +0100

    Sound System uncontrolled

---
+++ Assets/Scripts/Enviroment/GridMap.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridMap : MonoBehaviour
+{
+
+    const int gridMaxSize = 100;
+    const int cellMaxSize = 100;
+
+
+    [Range(1, gridMaxSize)]
+    public int xSize;
+
+    [Space(2)]
+    [Range(1, gridMaxSize)]
+    public int ySize;
+
+    [Space(3)]
+    [Range(0.1f, cellMaxSize)]
+    public float cellSize;
+
+    public Vector2[,] points;
+
+    [Space(4)]
+    [Range(0, 100)]
+    public float cubeSize;
+
+    public Color cubeColor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    private void OnDrawGizmos()
+    {
+        if ((!Application.isPlaying && transform.hasChanged) || points == null)
+            points = getPoints();
+
+        Vector3 gizmosSize = new Vector3(cubeSize, cubeSize, cubeSize);
+
+
+        Gizmos.color = cubeColor;
+        for (int i = 0; i < xSize; i++)
+        {
+            for (int j = 0; j < ySize; j++)
+            {
+
+                Gizmos.DrawCube(
+                    new Vector3(points[i, j].x, transform.position.y, points[i, j].y),
+                    gizmosSize);
+            }
+
+
+        }
+    }
+
+    public Vector2[,] getPoints()
+    {
+
+        Vector2[,] matrix = new Vector2[xSize, ySize];
+
+        for (int i = 0; i < xSize; i++)
+        {
+            //matrix[i] = new Vector3[ySize];
+
+            for (int j = 0; j < ySize; j++)
+            {
+                Vector3 pos = transform.position;
+
+                matrix[i, j].x = Mathf.Round(pos.x / cellSize) * cellSize + cellSize * i;
+                matrix[i, j].y = Mathf.Round(pos.z / cellSize) * cellSize + cellSize * j;
+            }
+        }
+
+
+        return matrix;
+    }
+
+    public Vector3 getGridPosition(Vector3 pos)
+    {
+        return new Vector3(Mathf.Round(pos.x / cellSize) * cellSize, transform.position.y, Mathf.Round(pos.z / cellSize) * cellSize);
+    }
+
+    public Vector3 getGridPositionWithOffset(Vector3 pos, int xOffset, int yOffset)
+    {
+        Vector3 aux = getGridPosition(pos); ;
+
+        aux.x += xOffset * cellSize;
+        aux.z += yOffset * cellSize;
+
+
+        return aux;
+    }
+
+}
---
+++ Assets/Scripts/Enviroment/SoundManager.cs
@@ -0,0 +1,264 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(GridMap))]
+public class SoundManager : MonoBehaviour
+{
+    public Transform cameraTransform;
+    GridMap myGridMap;
+
+    public AudioSource sourceReference;
+
+    [Range(1, 100)]
+    public int instances = 1;
+
+    AudioSource[] sources;
+
+    /*
+    * North -> Z+
+    * South -> Z-
+    * East  -> X+
+    * West  -> X-
+    */
+
+    public enum Direction
+    {
+        North,
+        South,
+        East,
+        West,
+        None
+    }
+
+    public enum PositionToPlayer
+    {
+        Behind,
+        Forward,
+        Right,
+        Left
+
+    }
+
+    private void Awake()
+    {
+        myGridMap = GetComponent<GridMap>();
+
+        sources = new AudioSource[instances];
+        sources[0] = sourceReference;
+
+        for (int i = 1; i < sources.Length; i++)
+        {
+            sources[i] = Instantiate(sourceReference);
+            sources[i].name = "Sound Manager Source " + i;
+            sources[i].transform.parent = sourceReference.transform;
+        }
+    }
+
+    Direction getFacingDirection()
+    {
+        Direction d = Direction.None;
+
+        if (cameraTransform != null)
+        {
+            //Y indicates Direction
+            float y = cameraTransform.rotation.eulerAngles.y;
+
+            //0 -> North
+            if (y > 315 || y <= 45)
+                d = Direction.North;
+            ////90 -> East
+            if (y > 45 && y <= 135)
+                d = Direction.East;
+
+            //180 -> South
+            if (y > 135 && y <= 225)
+                d = Direction.South;
+
+            //270 -> West
+            if (y > 225 && y <= 315)
+                d = Direction.West;
+
+        }
+
+        return d;
+
+
+    }
+
+    AudioSource getFreeAudioSource()
+    {
+        if (sources != null)
+            foreach (AudioSource s in sources)
+            {
+                if (s != null && !s.isPlaying)
+                {
+                    return s;
+                }
+            }
+
+        return null;
+    }
+
+    public bool playSoundOffset(int x, int y, AudioClip clip, AudioSource source)
+    {
+        if (source != null)
+        {
+            source.transform.position = myGridMap.getGridPositionWithOffset(cameraTransform.position, x, y);
+            source.clip = clip;
+            source.Play();
+
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
+    }
+
+    public bool playSoundInDirection(PositionToPlayer position, int range, AudioClip clip)
+    {
+        AudioSource source = getFreeAudioSource();
+
+        if (source != null)
+        {
+
+            Direction direction = getFacingDirection();
+            int x = 0;
+            int y = 0;
+
+
+            //x->x y->z
+            switch (direction)
+            {
+                case Direction.North:
+
+                    switch (position)
+                    {
+                        case PositionToPlayer.Forward:
+                            x = 0;
+                            y = 1;
+
+                            break;
+
+                        case PositionToPlayer.Behind:
+                            x = 0;
+                            y = -1;
+                            break;
+
+                        case PositionToPlayer.Right:
+                            x = 1;
+                            y = 0;
+                            break;
+
+                        case PositionToPlayer.Left:
+                            x = -1;
+                            y = 0;
+                            break;
+
+                    }
+
+                    break;
+
+                case Direction.South:
+
+                    switch (position)
+                    {
+                        case PositionToPlayer.Forward:
+                            x = 0;
+                            y = -1;
+                            break;
+
+                        case PositionToPlayer.Behind:
+                            x = 0;
+                            y = 1;
+                            break;
+
+                        case PositionToPlayer.Right:
+                            x = -1;
+                            y = 0;
+                            break;
+
+                        case PositionToPlayer.Left:
+                            x = 1;
+                            y = 0;
+                            break;
+
+                    }
+
+
+
+                    break;
+
+                case Direction.East:
+
+                    switch (position)
+                    {
+                        case PositionToPlayer.Forward:
+                            x = -1;
+                            y = 0;
+                            break;
+
+                        case PositionToPlayer.Behind:
+                            x = 1;
+                            y = 0;
+                            break;
+
+                        case PositionToPlayer.Right:
+                            x = 0;
+                            y = 1;
+                            break;
+
+                        case PositionToPlayer.Left:
+                            x = 0;
+                            y = -1;
+                            break;
+
+                    }
+
+                    break;
+
+                case Direction.West:
+
+                    switch (position)
+                    {
+                        case PositionToPlayer.Forward:
+                            x = 1;
+                            y = 0;
+                            break;
+
+                        case PositionToPlayer.Behind:
+                            x = -1;
+                            y = 0;
+                            break;
+
+                        case PositionToPlayer.Right:
+                            x = 0;
+                            y = -1;
+                            break;
+
+                        case PositionToPlayer.Left:
+                            x = 0;
+                            y = 1;
+                            break;
+
+                    }
+
+                    break;
+
+                default:
+                    break;
+            }
+
+            playSoundOffset(x * range, y * range, clip, source);
+            return true;
+
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+}
